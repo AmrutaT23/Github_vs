73c743662fe94ddc52524496a8570a2745c449fb
AmrutaT23 <thakareamruta2342@gmail.com>
2024-01-04 22:02:09 +0530

    Even or odd number from first 100 natural number

---
+++ Natural.java
@@ -0,0 +1,17 @@
+//Write a program to print the even or odd number from first 100 natural number.
+
+class Natural
+{
+	public static void main(String args[])
+	{
+		int i=2;
+		
+		while(i<=100)
+		{
+			System.out.print(i+" ");
+			i=i+2;
+		}
+	}
+}
+		
+		
